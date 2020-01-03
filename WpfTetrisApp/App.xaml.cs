32bfe0f0e984b6dba68acc588952f783dad89309
Filip Kosmala <kosmala.filip@gmail.com>
2020-01-03 18:12:25 +0100

    Moved Models to separate project. Prepared basic DI configuration.

--- WpfTetrisApp/App.xaml.cs
+++ WpfTetrisApp/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿using Prism.Ioc;
-using WpfTetrisApp.Views;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.IO;
 using System.Windows;
+using Unity;
+using Unity.Microsoft.DependencyInjection;
+using WpfTetrisApp.Views;
+using WpfTetrisLib.Models;
 
 namespace WpfTetrisApp
 {
@@ -9,14 +17,34 @@ namespace WpfTetrisApp
     /// </summary>
     public partial class App
     {
+        public IConfiguration Configuration { get; private set; }
+
+        private IUnityContainer _container;
         protected override Window CreateShell()
-        {
-            return Container.Resolve<MainWindow>();
+        { 
+            _container = Container.GetContainer();
+
+            var appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Tetris WPF\\config\\");
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile(Path.Combine(appDataPath, "appsettings.json"), true, true)
+                .Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _container.BuildServiceProvider(serviceCollection);
+            return _container.Resolve<MainWindow>();
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
 
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
         }
     }
 }
